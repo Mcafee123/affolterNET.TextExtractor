e8d4f06868e05c115cd89e951169aabfba6f988f
Martin Affolter <martin@affolter.net>
2024-01-24 10:54:33 +0100

    connected frontend and backend

--- api/HttpTriggers/Info.cs
+++ api/HttpTriggers/Info.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -16,15 +17,17 @@ public class Info
     }
 
     [Function("Info")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
 
         var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        // response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-        response.WriteString("Welcome to Azure Functions!");
+        dynamic obj = new ExpandoObject();
+        obj.text = "Welcome to Azure Functions!";
+        await response.WriteAsJsonAsync(obj as object);
 
         return response;
         
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,21 +1,27 @@
 <template lang="pug">
-h1 Extract Text from PDF
+h1 Extract Text from PDF: {{ msg }}
 ul
     li(@click="goToPdf('Verfuegung_Nr_23-24_24846_3.json')") Verfügung Yanik Zürcher
 </template>
 
 <script lang="ts" setup>
 
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const goToPdf = (filename: string) => {
-    const path = `/pdf/${encodeURI(filename)}`
-    router.push(path)
+  const path = `/pdf/${encodeURI(filename)}`
+  router.push(path)
 }
 
+const msg = ref<string>('')
+onMounted(async () => {
+  const { text } = await( await fetch(`/api/info`)).json();
+  msg.value = text
+})
 </script>
 
 <style lang="scss" scoped>
